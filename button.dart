80574110555ff20ebe47ca86b2b0a34ad38a2077
Ali <150606428+Inoyatovlee@users.noreply.github.com>
2023-11-16 10:54:38 +0500

    Add files via upload

---
+++ button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  String text;
+  Color color;
+  Function? click;
+  Button({
+    super.key,
+    required this.text,
+    required this.color,
+    this.click,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        if (click != null) {
+          click!();
+        }
+      },
+      child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+          decoration: BoxDecoration(
+              color: color, borderRadius: BorderRadius.circular(7)),
+          child: Center(
+            child: Text(
+              text,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600),
+            ),
+          )),
+    );
+  }
+}
